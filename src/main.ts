2e1617cfa93ebdb688847f70d2072d748b20f006
Evan Tardiff <10evantardiff@gmail.com>
2024-05-26 01:32:59 -0700

    fixed typo that was causing a checkered background

--- src/main.ts
+++ src/main.ts
@@ -2,29 +2,25 @@ import { k } from "./kaboomCtx";
 import { makeMap } from "./utils";
 
 async function gameSetup() {
-    k.loadSprite("assests", "./kirby-like.png", {
-       sliceX: 9,
-       sliceY: 10,
-       anims: {
-        kirbIdle: 0,
-        kirdInhaling: 1,
-        kirdFull: 2,
-        kirdInhaleEffect: { from: 3, to: 8, speed: 15, loop: true},
-        shootingStar: 9,
-        flame: { from: 36, to: 37, speed: 4, loop: true },
-        guyIdle: 18,
-        guyWalk: { from: 18, to: 19, speed: 4, loop: true },
-        bird: { from: 27, to: 28, speed: 4, loop: true },
+    k.loadSprite("assets", "./kirby-like.png", {
+        sliceX: 9,
+        sliceY: 10,
+        anims: {
+            kirbIdle: 0,
+            kirdInhaling: 1,
+            kirdFull: 2,
+            kirdInhaleEffect: { from: 3, to: 8, speed: 15, loop: true },
+            shootingStar: 9,
+            flame: { from: 36, to: 37, speed: 4, loop: true },
+            guyIdle: 18,
+            guyWalk: { from: 18, to: 19, speed: 4, loop: true },
+            bird: { from: 27, to: 28, speed: 4, loop: true },
         },
-     
     });
 
     k.loadSprite("level-1", "./level-1.png");
 
-    const { map: level1Layout, spawnPoints: level1SpawnPoints } = await makeMap(
-        k,
-        "level-1"
-      );
+    const { map: level1Layout, spawnPoints: level1SpawnPoints } = await makeMap(k, "level-1");
 
     k.scene("level-1", () => {
         k.setGravity(2100);
@@ -37,7 +33,7 @@ async function gameSetup() {
         k.add(level1Layout);
     });
 
-    k.go("level-1)");
+    k.go("level-1");
 }
 
 gameSetup();
